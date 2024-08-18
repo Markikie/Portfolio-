b869299c5d59dd4e637c1acb6aad37eca791835e
Cheer Chokwittaya <cheer.chokwit@gmail.com>
2024-08-18 16:09:20 -0500

    clean up

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,29 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+/**
+ * Color	Hex
+    Navy	#0a192f
+    Light Navy	#112240
+    Lightest Navy	#233554
+    Slate	#8892b0
+    Light Slate	#a8b2d1
+    Lightest Slate	#ccd6f6
+    White	#e6f1ff
+    Green	#64ffda
 
+   main
+   text-slate-200 => header
+   bg-teal-400/10
+
+
+ */
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
