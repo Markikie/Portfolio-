3b8f626651371ad1eef17533bae790b4560eaff2
Cheer Chokwittaya <cheer.chokwit@gmail.com>
2024-08-20 19:52:29 -0500

    update experience mock data

--- src/data/index.ts
+++ src/data/index.ts
@@ -29,18 +29,15 @@ Collaborated with cross-functional teams to identify and resolve platform issues
   },
   {
     id: '2',
-    startYear: '2021',
+    startYear: '2019',
     titles: [
       {
-        title: 'Software Engineer I',
-        endYear: '2023',
-      },
-      {
-        title: 'Software Engineer II',
+        title: 'Full Stack Developer',
+        endYear: '2021',
       },
     ],
-    company: 'Mediafly, Inc',
-    link: 'https://www.mediafly.com',
+    company: 'DESSS, Inc.',
+    link: 'https://www.desss.com',
     descriptions: `Developed Client and Admin sites using a mobile-first approach with React, creating a lightweight single-page application (SPA) with reusable components, and managed state with Redux to address prop drilling and maintain a single source of truth. Implemented Test-Driven Development (TDD) with Jest and Supertest to ensure clean code and monitor functional side effects. Architected a Node.js server with Express, designed REST APIs for CRUD operations, and implemented reusable middleware for JWT authorization. Designed relational database schemas using PostgreSQL and Sequelize, ensuring data consistency through complex relationships. Set up CI/CD pipelines using GitHub Actions for deployment to AWS Elastic Beanstalk, and documented API routes and structures with Swagger for clear communication and consistency.`,
     technologies: [
       'JavaScript',
@@ -56,18 +53,15 @@ Collaborated with cross-functional teams to identify and resolve platform issues
   },
   {
     id: '3',
-    startYear: '2021',
+    startYear: '2018',
     titles: [
       {
-        title: 'Software Engineer I',
-        endYear: '2023',
-      },
-      {
-        title: 'Software Engineer II',
+        title: 'Full Stack Developer',
+        endYear: '2019',
       },
     ],
-    company: 'Mediafly, Inc',
-    link: 'https://www.mediafly.com',
+    company: 'Nemaco Technology',
+    link: 'https://www.nemaco.com/',
     descriptions: `Designed the main website and admin site using Figma, collaborating closely with engineering, product teams, and the CEO to refine and implement designs. Developed Single Page Applications (SPA) using Angular, JavaScript, HTML5, and CSS3 (Bootstrap), leveraging Angular's component-based architecture for efficient and maintainable code. Created RESTful services with Node.js and Express, and connected to MongoDB (Atlas) for data storage, defining models and handling complex data relationships. Deployed applications to Heroku for scalable and reliable hosting, and utilized AWS S3 for high-availability file storage.`,
     technologies: [
       'JavaScript',
